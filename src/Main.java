5b95edccc6268cd9eceef76d8471f4fc601fbfc4
Filson14 <fpasternak14@gmail.com>
2015-05-19 17:41:51 +0200

    Main
    Teoretycznie dobry main wykomentowany.
    Aktualny main sprawdza działanie nowej wersji skanera, wypisuje numery
    tokenow na ekran.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,29 @@
 import java.io.*;
 
+import java_cup.runtime.*;
+
 public class Main {
 
 	static boolean do_debug_parse = false;
 	public static void main(String[] args) {
+		/*
 		try{
-			Parser p = new Parser(new Scanner(System.in));
-			Object result = p.parse().value;
+			Parser p = new Parser(new Scanner(new FileReader("test/test.txt")));
+			Object result = p.parse();
 		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
 			
+		}
+		*/
+		Scanner scaner = new Scanner(System.in);
+		Symbol s;
+		try {
+			while((s = scaner.next_token()) != null){
+				System.out.println(s.sym);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 
 	}
